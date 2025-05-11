1bddacbbdeebc4f75881b58f9af3d1cb23634fb2
servomouse <servomouse@protonmail.com>
2025-05-10 18:37:22 -0600

    Make keyboard autogenerated

--- virtual_keyboard/script.js
+++ virtual_keyboard/script.js
@@ -1,15 +1,15 @@
 let layouts = {
     "EN": [
-        [["key", "1"], ["key", "2"], ["key", "3"], ["key", "4"], ["key", "5"], ["key", "6"], ["key", "7"], ["key", "8"], ["key", "9"], ["key", "0"], ["key control delete", "Backspace"]],
-        [["key", "Q"], ["key", "W"], ["key", "E"], ["key", "R"], ["key", "T"], ["key", "Y"], ["key", "U"], ["key", "I"], ["key", "O"], ["key", "P"]],
-        [["key", "A"], ["key", "S"], ["key", "D"], ["key", "F"], ["key", "G"], ["key", "H"], ["key", "J"], ["key", "K"], ["key", "L"]],
-        [["key", "Z"], ["key", "X"], ["key", "C"], ["key", "V"], ["key", "B"], ["key", "N"], ["key", "M"]],
+        [["kb-button", "1"], ["kb-button", "2"], ["kb-button", "3"], ["kb-button", "4"], ["kb-button", "5"], ["kb-button", "6"], ["kb-button", "7"], ["kb-button", "8"], ["kb-button", "9"], ["kb-button", "0"], ["kb-button control delete", "Backspace"]],
+        [["kb-button", "Q"], ["kb-button", "W"], ["kb-button", "E"], ["kb-button", "R"], ["kb-button", "T"], ["kb-button", "Y"], ["kb-button", "U"], ["kb-button", "I"], ["kb-button", "O"], ["kb-button", "P"]],
+        [["kb-button", "A"], ["kb-button", "S"], ["kb-button", "D"], ["kb-button", "F"], ["kb-button", "G"], ["kb-button", "H"], ["kb-button", "J"], ["kb-button", "K"], ["kb-button", "L"]],
+        [["kb-button", "Z"], ["kb-button", "X"], ["kb-button", "C"], ["kb-button", "V"], ["kb-button", "B"], ["kb-button", "N"], ["kb-button", "M"]],
     ],
     "RU": [
-        [["key", "Ё"], ["key", "1"], ["key", "2"], ["key", "3"], ["key", "4"], ["key", "5"], ["key", "6"], ["key", "7"], ["key", "8"], ["key", "9"], ["key", "0"], ["key control delete", "Backspace"]],
-        [["key", "Й"], ["key", "Ц"], ["key", "У"], ["key", "К"], ["key", "Е"], ["key", "Н"], ["key", "Г"], ["key", "Ш"], ["key", "Щ"], ["key", "З"], ["key", "Х"], ["key", "Ъ"]],
-        [["key", "Ф"], ["key", "Ы"], ["key", "В"], ["key", "А"], ["key", "П"], ["key", "Р"], ["key", "О"], ["key", "Л"], ["key", "Д"], ["key", "Ж"], ["key", "Э"]],
-        [["key", "Я"], ["key", "Ч"], ["key", "С"], ["key", "М"], ["key", "И"], ["key", "Т"], ["key", "Ь"], ["key", "Б"], ["key", "Ю"]],
+        [["kb-button", "Ё"], ["kb-button", "1"], ["kb-button", "2"], ["kb-button", "3"], ["kb-button", "4"], ["kb-button", "5"], ["kb-button", "6"], ["kb-button", "7"], ["kb-button", "8"], ["kb-button", "9"], ["kb-button", "0"], ["kb-button control delete", "Backspace"]],
+        [["kb-button", "Й"], ["kb-button", "Ц"], ["kb-button", "У"], ["kb-button", "К"], ["kb-button", "Е"], ["kb-button", "Н"], ["kb-button", "Г"], ["kb-button", "Ш"], ["kb-button", "Щ"], ["kb-button", "З"], ["kb-button", "Х"], ["kb-button", "Ъ"]],
+        [["kb-button", "Ф"], ["kb-button", "Ы"], ["kb-button", "В"], ["kb-button", "А"], ["kb-button", "П"], ["kb-button", "Р"], ["kb-button", "О"], ["kb-button", "Л"], ["kb-button", "Д"], ["kb-button", "Ж"], ["kb-button", "Э"]],
+        [["kb-button", "Я"], ["kb-button", "Ч"], ["kb-button", "С"], ["kb-button", "М"], ["kb-button", "И"], ["kb-button", "Т"], ["kb-button", "Ь"], ["kb-button", "Б"], ["kb-button", "Ю"]],
     ],
 }
 
@@ -21,16 +21,25 @@ let textContainer = document.querySelector(".textContainer");
 // let allKey = document.querySelectorAll(".key");
 let isCaps = false;
 let currentLang = "EN";
+let userText = "";
 
 function addSymbol(text) {
-    let userText = textContainer.innerText;
-    textContainer.innerText = userText + text;
+    // let userText = textContainer.innerText;
+    userText += text;
+    textContainer.innerText = userText;
 }
 
 function handleControlKey(key) {
+    console.log(`Processing control key ${key}`);
     if(key === "Backspace") {
-        let userText = textContainer.innerText;
-        textContainer.innerText = userText.substring(0, userText.length - 1);
+        // let userText = textContainer.innerText;
+        console.log(`Initial text content: ${textContainer.innerText}`);
+        if(userText.length > 0) {
+            userText = userText.substring(0, userText.length - 1);
+            console.log(`Updated text: ${userText}`);
+            textContainer.innerText = userText;
+            console.log(`Updated text content: ${textContainer.innerText}`);
+        }
     } else if(key === "Enter") {
         addSymbol("\n");
     } else if(key === " ") {
@@ -57,30 +66,35 @@ function updateKeyboard(lang) {
     kb.innerHTML = "";
     let newKb = ""
     // Add keys from layout:
-    console.log(layouts[currentLang].length);
+    let isFirstRow = true;
     
     for(let row=0; row<layouts[currentLang].length; row++) {
-        newKb += '<div class="row">';
+        if(isFirstRow) {
+            newKb += '<div class="row first">';
+            isFirstRow = false;
+        } else {
+            newKb += '<div class="row">';
+        }
         for(let key=0; key<layouts[currentLang][row].length; key++) {
-            console.log(`<div class="${layouts[currentLang][row][key][0]}">${layouts[currentLang][row][key][1]}</div>`)
-            newKb += `<div class="${layouts[currentLang][row][key][0]}">${layouts[currentLang][row][key][1]}</div>`;
+            // console.log(`<div class="${layouts[currentLang][row][key][0]}">${layouts[currentLang][row][key][1]}</div>`)
+            newKb += `<button class="${layouts[currentLang][row][key][0]}">${layouts[currentLang][row][key][1]}</button>`;
         }
         newKb += '</div>';
     }
 
-    let lastRow = '<div class="row"><div class="key control capslock">CapsLock</div><div class="dropup">'
+    let lastRow = '<div class="row"><button class="kb-button control capslock">CapsLock</button><div class="dropup">'
     lastRow += `<button class="dropbtn">${currentLang}</button><div class="dropup-content">`;
     const langs = Object.keys(layouts);
     for(let i = 0; i < langs.length; i++) {
         lastRow += `<a href="#" onclick="updateLanguage('${langs[i]}')">${langs[i]}</a>`;
     }
-    lastRow += '</div></div><div class="key control space"></div><div class="key control enter">Enter</div></div>';
+    lastRow += '</div></div><button class="kb-button control space"></button><button class="kb-button control enter">Enter</button></div>';
     newKb += lastRow;
-    console.log(newKb);
+    // console.log(newKb);
     kb.innerHTML = newKb;
 
     // Add event listeners for on-screen keyboard clicks (similar to your existing code)
-    for (let key of document.querySelectorAll(".key")) {
+    for (let key of document.querySelectorAll(".kb-button")) {
         key.addEventListener("click", function () {
             if(key.classList.contains("delete")) {
                 handleControlKey("Backspace");
@@ -99,7 +113,7 @@ function updateKeyboard(lang) {
 
 // Language selector button:
 function updateLanguage(newLang) {
-    console.log(newLang);
+    // console.log(newLang);
     updateKeyboard(newLang);
 }
 
