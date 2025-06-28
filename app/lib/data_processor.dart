e95fe54e3dd32e52d13f813e452ca4b03f88e85d
servomouse <servomouse@protonmail.com>
2025-06-27 18:31:03 -0600

    Add QuickAlert

--- app/lib/data_processor.dart
+++ app/lib/data_processor.dart
@@ -1,9 +1,65 @@
+import 'package:quickalert/quickalert.dart';
 
 mixin DataProcessor {
-  var theWord = "hello";
-  var answer = "";
-  var sentence = "Hello World!";
-  var redWord = "Hello";
+
+  Map<String, dynamic> task = {
+    "baseWord": "hello",
+    "redWord": "Hello",
+    "answer": "hola",
+    "sentence": "Hello World!"
+  };  // Cause dictionaries are good
+
   var correctCounter = 0;
   var incorrectCounter = 0;
+  var result = 0.0;
+
+  var dictionary;
+  List<String> pairs = ["en-ru", "en-sp"];
+  var selectedPair = "en-sp";
+
+  void notifyListeners(); // Defined in the ChangeNotifier class
+
+  void shouAlert(cntx, alert) {
+    if(alert == "success") {
+      QuickAlert.show(
+        context: cntx, 
+        type: QuickAlertType.success,
+        title: "Correct!"
+      );
+    } else if(alert == "error") {
+      QuickAlert.show(
+        context: cntx, 
+        type: QuickAlertType.error,
+        title: "Correct answer is \"${task["answer"]}\"!"
+      );
+    }
+  }
+
+  void processUserInput(cntx, input) {
+    if(input.toLowerCase() == task["answer"].toLowerCase()) {
+      correctCounter ++;
+      shouAlert(cntx, "success");
+    } else {
+      incorrectCounter ++;
+      shouAlert(cntx, "error");
+    }
+    if(correctCounter > 0) {
+      if(incorrectCounter > 0) {
+        result = 100 * (correctCounter / (correctCounter + incorrectCounter));
+      } else {
+        result = 100.0;
+      }
+    } else {
+      if(incorrectCounter > 0) {
+        result = 0.0;
+      } else {
+        result = 0.0;
+      }
+    }
+    notifyListeners();
+  }
+
+  void updateSelectedPair(pair) {
+    selectedPair = pair;
+  }
 }
