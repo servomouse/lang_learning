f2d5bb8663745fd5e49e763fce075cc55ac81808
servomouse <servomouse@protonmail.com>
2025-11-30 10:44:28 -0600

    Implementing the login functionality

--- web_app/main.py
+++ web_app/main.py
@@ -17,13 +17,14 @@ class AppData:
             with open(SCORES_FILE, 'r', encoding='utf-8') as file:
                 self.scores = json.load(file)
         else:
-            with open(SCORES_FILE, 'w', encoding='utf-8') as file:
-                json.dump({}, file, ensure_ascii=False, indent=4)
+            self.scores = {}
+            self.save_scores()
 
         if os.path.isfile(USERS_FILE):
             with open(USERS_FILE, 'r', encoding='utf-8') as file:
                 self.users = json.load(file)
         else:
+            self.users = {}
             self.save_users()
     
     def get_dictionary(self):
@@ -39,10 +40,13 @@ class AppData:
     # Scores:
     
     def update_score(self, langs, word, new_score):
-        key = f"{langs[0]}-{langs[1]}-{word}"
         if self.current_user not in self.scores:
             self.scores[self.current_user] = {}
-        self.scores[self.current_user][key] = new_score
+        if langs[0] not in self.scores[self.current_user]:
+            self.scores[self.current_user] = langs[0]
+        if langs[1] not in self.scores[self.current_user][langs[0]]:
+            self.scores[self.current_user][langs[0]] = langs[1]
+        self.scores[self.current_user][langs[0]][langs[1]][word] = new_score
         self.save_scores()
 
     def get_scores(self):
@@ -73,20 +77,54 @@ class AppData:
 
 
 app = Flask(__name__)
-dict = AppData()
+app_data = AppData()
 
 @app.route('/api/dictionary', methods=['GET'])
 def get_dictionary():
-    dictionary = dict.get_dictionary()
+    dictionary = app_data.get_dictionary()
     return jsonify(dictionary)
+
+@app.route('/api/scores', methods=['GET'])
+def get_scores():
+    scores = app_data.get_scores()
+    return jsonify(scores)
 
 @app.route('/api/update_score', methods=['POST'])
 def update_score():
-    data = request.json
+    data = request.get_json()
     # language = data.get('language')
     # word = data.get('word')
     # is_correct = data.get('is_correct')
     print(f"Received data: {data}")
+
+@app.route('/api/add_user', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    print(f"Add user received data: {data}")
+
+    res = app_data.add_user(username, password)
+
+    if res == "User doesn't exist":
+        return jsonify({"status": "error", "message": "User doesn't exist"}), 404
+
+    if res == "Incorrect password":
+        return jsonify({"status": "error", "message": "Incorrect password"}), 401
+
+    return jsonify({"status": "success", "message": "Login successful"}), 200
+    # language = data.get('language')
+    # word = data.get('word')
+    # is_correct = data.get('is_correct')
+    print(f"Add user received data: {data}")
+
+@app.route('/api/verify_user', methods=['POST'])
+def verify_user():
+    data = request.get_json()
+    # language = data.get('language')
+    # word = data.get('word')
+    # is_correct = data.get('is_correct')
+    print(f"Verify user received data: {data}")
 
 @app.route('/')
 def serve_index():
